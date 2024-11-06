8f8fd8a665cb96e8121ea2c967c85e1207b561b7
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2024-11-06 14:56:30 +0300

    Update Person.java

--- Iteration 1/Source Code/Person.java
+++ Iteration 1/Source Code/Person.java
@@ -1,1 +1,26 @@
+import java.util.Date;
 
+public abstract class Person {
+     protected  String name;
+     protected  String surname;
+     protected  Date birthdate;
+     protected  char Gender;
+
+
+   public String getName(){
+        return name;
+
+   };
+
+   public String getSurname(){
+        return surname;
+   };
+
+     public Date getBirthdate() {
+          return birthdate;
+     }
+
+     public char getGender() {
+          return Gender;
+     }
+}
