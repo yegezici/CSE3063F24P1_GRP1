4cd988bd32562c47cd86c0d1ca1081a3f4cc537f
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-06 14:58:48 +0300

    Update Course.java

--- Iteration 1/Source Code/Course.java
+++ Iteration 1/Source Code/Course.java
@@ -1,1 +1,12 @@
+public class Course {
+    private String courseId;
+    private String courseName;
+
+    public Course(){}
+
+    public Course(String courseId, String courseName){
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+}
 
