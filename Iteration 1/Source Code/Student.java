622447d3f7f1249b17acc3393b328597d2f294d2
Alperen Burak Koçyiğit <116749326+AlBuKo@users.noreply.github.com>
2024-11-08 15:11:08 +0300

    Update Student.java

--- Iteration 1/Source Code/Student.java
+++ Iteration 1/Source Code/Student.java
@@ -1,22 +1,41 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+
 public class Student extends Person{
-    private Transcript transcript;
-    private String studentID;
-    private Advisor advisor;
-
-    public void registerCourse(Course course) {
-
-
+	private Transcript transcript;
+	private String studentID;
+	private Advisor advisor;
+	private List<Course> registeredCourse;
+	
+	
+	public Student(String name, String surname, Date birthdate, char Gender, String studentID) {
+        this.name = name;
+        this.surname = surname;
+        this.birthdate = birthdate;
+        this.Gender = Gender;
+        this.advisor = advisor;
+        this.transcript = transcript;
+        this.studentID = studentID;
+        registeredCourse = new ArrayList<>(); 
     }
+	
+	public void registerCourse(Course course) {
+	
+		
+	}
 
-    public Transcript getTranscript() {
-        return transcript;
-    }
+	public Transcript getTranskript() {
+		return transcript;
+	}
 
-    public String getStudentID() {
-        return studentID;
-    }
+	public String getStudentID() {
+		return studentID;
+	}
 
-    public Advisor getAdvisor() {
-        return advisor;
-    }
+	public Advisor getAdvisor() {
+		return advisor;
+	}
+	
 }
