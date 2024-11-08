805a9337a89a0f6c021146f2116c19f2b1e469f6
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 14:54:40 +0300

    Update Course.java

--- Iteration 1/Source Code/Course.java
+++ Iteration 1/Source Code/Course.java
@@ -1,12 +1,43 @@
+import java.util.ArrayList;
+
 public class Course {
     private String courseId;
     private String courseName;
+    private Lecturer lecturer;
+    private ArrayList<Student> students;
+    private ArrayList<CourseSection> courseSections;
 
-    public Course(){}
+    public Course() {
+    }
 
-    public Course(String courseId, String courseName){
+    public Course(String courseId, String courseName) {
         this.courseId = courseId;
         this.courseName = courseName;
     }
-}
 
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void addStudent(Student student) {
+        try {
+            if (student != null)
+                students.add(student);
+            else
+                System.out.println("The student object is null");
+        } catch (NullPointerException e) {
+            System.out.println("The students list has not been initialized.");
+        }catch (Exception e1){
+            System.out.println(e1.getMessage());
+        }
+    }
+
+}
