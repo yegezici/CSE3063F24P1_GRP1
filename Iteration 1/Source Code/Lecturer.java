12e92143a2cba79e72c73c1ba7f1da2e29b00c6b
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 15:47:25 +0300

    Update Lecturer.java

--- Iteration 1/Source Code/Lecturer.java
+++ Iteration 1/Source Code/Lecturer.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Lecturer extends Staff{
     protected ArrayList<Course> courses;
 
     public Lecturer (){
-
+        super();
+    }
+    public Lecturer(String name, String surname){
+        super(name,surname);
+    }
+    public Lecturer(String name, String surname, Date birthdate, char gender){
+        super(name, surname, birthdate, gender);
     }
 
     public ArrayList<Course> getCourses(){
