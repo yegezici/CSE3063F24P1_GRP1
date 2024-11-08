794bbd9844a6cc562d313bd3cd69853950812b97
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 15:17:27 +0300

    Update Transcript.java

--- Iteration 1/Source Code/Transcript.java
+++ Iteration 1/Source Code/Transcript.java
@@ -3,13 +3,17 @@ import java.util.ArrayList;
 public class Transcript {
     private ArrayList<Course> completedCourses;
     private ArrayList<Course> currentCourses;
+    private ArrayList<Course> waitedCourses;
 
-    Transcript(){};
+    public Transcript(){
 
-    Transcript(ArrayList<Course> comp, ArrayList<Course> current){
+    };
+
+    public Transcript(ArrayList<Course> comp, ArrayList<Course> current){
         this.completedCourses = comp;
         this.currentCourses = current;
     };
+
     public void addCompletedCourse(Course course){
         try {
             if (course != null)
@@ -35,4 +39,17 @@ public class Transcript {
             System.out.println(e1.getMessage());
         }
     }
+    public void addWaitedCourse(Course course){
+        try {
+            if (course != null)
+                waitedCourses.add(course);
+            else
+                System.out.println("The course object is null");
+        } catch (NullPointerException e) {
+            System.out.println("The waitedCourses list has not been initialized.");
+        }catch (Exception e1){
+            System.out.println(e1.getMessage());
+        }
+    }
+
 }
