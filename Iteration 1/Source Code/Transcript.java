288b0a55bc14fc8aae01d348986c35ae3cfc5eeb
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2024-11-08 14:32:07 +0300

    Create Transcript.java

---
+++ Iteration 1/Source Code/Transcript.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Transcript {
+    private ArrayList<Course> completedCourses;
+    private ArrayList<Course> currentCourses;
+
+    Transcript(){};
+
+    Transcript(ArrayList<Course> comp, ArrayList<Course> current){
+        this.completedCourses = comp;
+        this.currentCourses = current;
+    };
+   public void addCourse(ArrayList<Course> current){
+       
+   }
+}
