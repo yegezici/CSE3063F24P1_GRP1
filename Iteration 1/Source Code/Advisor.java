7ab8f5a21c94388f09c1551efe4ffde12fc0947a
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 15:48:02 +0300

    Update Advisor.java

--- Iteration 1/Source Code/Advisor.java
+++ Iteration 1/Source Code/Advisor.java
@@ -1,16 +1,32 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Advisor extends Lecturer {
     private ArrayList<Student> students;
 
-    public Advisor(){
+    public Advisor() {
 
     }
+    public Advisor(String name, String surname){
+      super(name,surname);
+    }
 
-    public void approveCourse(Student student, Course course){
-        try{
+    public Advisor(String name, String surname, Date birthdate, char gender){
+        super(name, surname, birthdate, gender);
+    }
+
+    public void approveCourse(Student student, Course course) {
+        try {
             student.getTranscript().addCurrentCourse(course);
-        }catch(Exception e){
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void addStudent(Student student) {
+        try {
+            students.add(student);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
