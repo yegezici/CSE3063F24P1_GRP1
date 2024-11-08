2cf3fe1a7e4f238280121a8aab6661dafca158d3
hasanerzz <133280711+hasanerzz@users.noreply.github.com>
2024-11-08 15:46:40 +0300

    Update CourseRegistration.java

--- Iteration 1/Source Code/CourseRegistration.java
+++ Iteration 1/Source Code/CourseRegistration.java
@@ -1,21 +1,37 @@
+
+import java.util.Date;
 import java.util.Scanner;
 
 public class CourseRegistration {
-    //Student student = new Student("Ali", "Kocyigit", )
+   // Student student = new Student("Ali", "Kocyigit",new Date(2002, 9, 18),'M',new  )
+
     public static void main(String[] args) {
         String pass = "abc123";
         String userid = "o150121031";
         while (true) {
-            //Login function
+            Scanner scan = new Scanner(System.in);
+            //Login usage
             login(userid, pass);
             if (userid.charAt(0) == 'o')
-                System.out.println("Hello Hasan Erz. Check your transcript and register a course that are available for you.");
+                System.out.println("Hello Hasan Erz.");
+                System.out.println("1. Transcript\n2. Register for course\n3.Log out");
+                int choice = scan.nextInt();
+                switch (choice){
+                    case 1: break;
+                    case 2: break;
+                    case 3:
+                        System.out.println("You are successfully logged out.\n");
+                        login(userid,pass);
+                }
+
+
 
 
         }
     }
-
+//login function
      private static void login(String userid, String password){
+        while(true){
          Scanner idscan = new Scanner(System.in);
          Scanner passwordscan = new Scanner(System.in);
          System.out.println("Please enter your User ID and Password");
@@ -23,12 +39,10 @@ public class CourseRegistration {
          String id = idscan.nextLine();
          System.out.println("Password: ");
          String pass = passwordscan.nextLine();
-         if(!(userid.equals(id)) && !(pass.equals(password)))
+         if(!(userid.equals(id)) && !(pass.equals(password))){
              System.out.println("Wrong username or password. Please try again");
-     }
+         } else
+             break;
 
+     }}
 
-
-
-
-}
