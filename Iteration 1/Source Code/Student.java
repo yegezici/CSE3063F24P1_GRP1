bdfc6ccad75aca73f82f6a3d88974b8bd309ea20
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 15:36:41 +0300

    Update Student.java

--- Iteration 1/Source Code/Student.java
+++ Iteration 1/Source Code/Student.java
@@ -4,15 +4,15 @@ import java.util.Date;
 
 
 public class Student extends Person{
-	private Transcript transcript;
-	private String studentID;
-	private Advisor advisor;
-	private ArrayList<Course> registeredCourse;
-	
-	public Student() {
-		
-	}
-	public Student(String name, String surname, Date birthdate, char Gender, Transcript transcript, Advisor advisor, String studentID) {
+    private Transcript transcript;
+    private String studentID;
+    private Advisor advisor;
+    private ArrayList<Course> registeredCourse;
+
+    public Student() {
+
+    }
+    public Student(String name, String surname, Date birthdate, char Gender, Transcript transcript, Advisor advisor, String studentID) {
         this.name = name;
         this.surname = surname;
         this.birthdate = birthdate;
@@ -20,24 +20,24 @@ public class Student extends Person{
         this.advisor = advisor;
         this.transcript = transcript;
         this.studentID = studentID;
-        registeredCourse = new ArrayList<>(); 
+        registeredCourse = new ArrayList<>();
     }
-	
-	public void registerCourse(Course course) {
-		
-		
-	}
 
-	public Transcript getTranskript() {
-		return transcript;
-	}
+    public void registerCourse(Course course) {
 
-	public String getStudentID() {
-		return studentID;
-	}
 
-	public Advisor getAdvisor() {
-		return advisor;
-	}
-	
+    }
+
+    public Transcript getTranscript() {
+        return transcript;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
 }
