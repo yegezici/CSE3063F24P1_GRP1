eca3b4fa8bdccd564832135ed9cdc7e05513c6ee
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 15:47:06 +0300

    Update Staff.java

--- Iteration 1/Source Code/Staff.java
+++ Iteration 1/Source Code/Staff.java
@@ -1,9 +1,22 @@
-public class Staff {
+import java.util.Date;
+
+public class Staff extends Person {
     protected String ssn;
 
-    public Staff(){}
+    public Staff() {
+        super();
+    }
 
-    public Staff(String ssn){
+    public Staff(String name, String surname) {
+        super(name, surname);
+    }
+
+    public Staff(String name, String surname, Date birthdate, char gender) {
+        super(name, surname, birthdate, gender);
+    }
+
+
+    public Staff(String ssn) {
         this.ssn = ssn;
     }
 }
