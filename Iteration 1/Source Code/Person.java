3365cd06490c1fd1e8e53dfaefda33b73ed333ab
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-11-08 15:46:49 +0300

    Update Person.java

--- Iteration 1/Source Code/Person.java
+++ Iteration 1/Source Code/Person.java
@@ -1,26 +1,39 @@
 import java.util.Date;
 
 public abstract class Person {
-     protected  String name;
-     protected  String surname;
-     protected  Date birthdate;
-     protected  char Gender;
+    protected  String name;
+    protected  String surname;
+    protected  Date birthdate;
+    protected  char gender;
 
+    public Person(){
 
-   public String getName(){
+    }
+    public Person(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    public Person(String name, String surname, Date birthdate, char gender){
+        this.name = name;
+        this.surname = surname;
+        this.birthdate = birthdate;
+        this.gender = gender;
+    }
+
+    public String getName(){
         return name;
 
-   };
+    };
 
-   public String getSurname(){
+    public String getSurname(){
         return surname;
-   };
+    };
 
-     public Date getBirthdate() {
-          return birthdate;
-     }
+    public Date getBirthdate() {
+        return birthdate;
+    }
 
-     public char getGender() {
-          return Gender;
-     }
+    public char getGender() {
+        return gender;
+    }
 }
