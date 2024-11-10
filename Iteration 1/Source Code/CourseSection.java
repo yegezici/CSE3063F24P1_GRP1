140512bbc9e27568418587e3897e4ea40c620d57
cihatemrevrds <emrevardis@gmail.com>
2024-11-10 16:38:39 +0300

    CourseSection Class Edit

--- Iteration 1/Source Code/CourseSection.java
+++ Iteration 1/Source Code/CourseSection.java
@@ -1,8 +1,21 @@
 
-public class CourseSection {
-   private Course course;
-   private String section;
-   private int sectionID;
+public class CourseSection extends Course{
+   private int sectionNumber;
 
+   public CourseSection(){
+      super();
+   }
 
+   public CourseSection(String courseId, String courseName){
+      super(courseId, courseName);
+   }
+
+   public CourseSection(String courseId, String courseName, int credits){
+      super(courseId, courseName, credits);
+   }
+
+   public CourseSection(String courseId, String courseName, int credits, int sectionNumber){
+      super(courseId, courseName, credits);
+      this.sectionNumber = sectionNumber;
+   }
 }
