bdfe69cb8d454c127fb7b319b32565564aa73544
Alperen Burak Koçyiğit <116749326+AlBuKo@users.noreply.github.com>
2024-11-11 19:30:28 +0300

    Update Person.java

--- Iteration 1/Source Code/Person.java
+++ Iteration 1/Source Code/Person.java
@@ -13,26 +13,27 @@ public abstract class Person {
         this.name = name;
         this.surname = surname;
     }
+    //constructor
     public Person(String name, String surname, Date birthdate, char gender){
         this.name = name;
         this.surname = surname;
         this.birthdate = birthdate;
         this.gender = gender;
     }
-
+//Retrieves the name of the person
     public String getName(){
         return name;
 
     };
-
+//Retrieves the surname of the person
     public String getSurname(){
         return surname;
     };
-
+//Retrieves the birth date of the person
     public Date getBirthdate() {
         return birthdate;
     }
-
+//Retrieves the gender of the person
     public char getGender() {
         return gender;
     }
