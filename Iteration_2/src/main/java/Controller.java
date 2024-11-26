51471f7e06ab923f5a639f199ac98d2ef8a944f5
AlBuKo <alperen3kcygt8@gmail.com>
2024-11-26 19:41:56 +0300

    update

--- Iteration_2/src/main/java/Controller.java
+++ Iteration_2/src/main/java/Controller.java
@@ -2,6 +2,6 @@ public class Controller {
     public static void main(String[] args) {
         CourseRegistration courseReg = new CourseRegistration();
         courseReg.init(); 
-       System.out.println(courseReg.students.get(0).getName()); 
+      
     }
 }
