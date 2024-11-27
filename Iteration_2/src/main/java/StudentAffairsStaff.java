efca0aad43ced33fcf48c217c03114fe8c3767af
yegezici <yegezici@gmail.com>
2024-11-27 16:02:38 +0300

    StudentAffairsClass

---
+++ Iteration_2/src/main/java/StudentAffairsStaff.java
@@ -0,0 +1,18 @@
+public class StudentAffairsStaff {
+
+    public StudentAffairsStaff(){
+
+    }
+
+    public void createCourses(){
+
+    }
+    public void removeCourses(){
+        
+    }
+    public void createCourseSection(){
+
+    }
+
+
+}
