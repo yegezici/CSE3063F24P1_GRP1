37d626925a61892fdcbbb5fa2a1e8403679a0802
hasanerzz <m.h.erzincanli@gmail.com>
2024-11-27 16:33:31 +0300

    TimeSlot

--- Iteration_2/src/main/java/CourseSection.java
+++ Iteration_2/src/main/java/CourseSection.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class CourseSection{
    private String sectionID;
    private TimeSlot timeSlot;
-   private String classroom;
    private ArrayList<Student> waitList;
    private int capacity;
    private Lecturer lecturer;
@@ -11,11 +10,14 @@ public class CourseSection{
    public CourseSection(){
       
    }
-   public CourseSection(String sectionId, String classroom, int capacity, Lecturer lecturer){
+   public CourseSection(String sectionId, int capacity, Lecturer lecturer){
      this.sectionID = sectionId;
      this.capacity = capacity;
-     this.classroom = classroom;
      this.lecturer = lecturer;
+   }
+
+   public void assignLecturer(){
+      
    }
    
    
---
+++ Iteration_2/src/main/java/TimeSlot.java
@@ -0,0 +1,16 @@
+public class TimeSlot {
+   private String timeInterval;
+   private String classroom; 
+
+   public TimeSlot(){
+
+   }
+   public TimeSlot(String timeInterval, String classroom){
+    this.classroom = classroom;
+    this.timeInterval = timeInterval;
+   }
+
+   public void assignTimeSlot(){
+    
+   }
+}
