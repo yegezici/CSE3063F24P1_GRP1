54db2006a344a637931b68f7d378bdc0345212cd
AlBuKo <alperen3kcygt8@gmail.com>
2024-11-29 23:32:26 +0300

    added weeklyschedule class

---
+++ Iteration_2/src/main/java/WeeklySchedule.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WeeklySchedule {
+
+    public WeeklySchedule(){
+    }
+
+    public void printStudentWeeklySchedule(Student student) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        for (String day : days) {
+            boolean dayPrinted = false;
+            for (CourseSection section : student.getTranscript().getCurrentSections()) {
+                if (section.getTimeSlots() != null) { 
+                    for (TimeSlot timeSlot : section.getTimeSlots()) {
+                        String timeInterval = timeSlot.getTimeInterval();
+                        System.out.println("Time slot interval: " + timeInterval); 
+    
+                        if (timeInterval != null && timeInterval.contains(day)) { 
+                            if (!dayPrinted) {
+                                System.out.println(day + ":");
+                                dayPrinted = true;
+                            }
+                            String timeDetails = timeInterval.replace(day + " ", "");
+                            String courseName = section.getParentCourse().getCourseName();
+                            System.out.println("  " + courseName + "  " + timeDetails);
+                        }
+                    }
+                } else {
+                    System.out.println("No time slots available for section " + section.getSectionID());
+                }
+            }
+        }
+    }
+    
+    
+    }   
