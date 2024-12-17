20069ad66765507e1b959d62652d3f42d97aab95
Yunus Emre Gezici <yegezici@gmail.com>
2024-12-17 20:44:09 +0300

    initsectionswithouttimeslotbecauseitdoesnothaveanytableyet

--- Iteration_3/SQLiteManagement.py
+++ Iteration_3/SQLiteManagement.py
@@ -8,8 +8,8 @@ from TimeSlot import TimeSlot
 class SqliteManager:
 
     def __init__(self):
-        conn = sqlite3.connect('Iteration_3/database/test.db')
-        cursor = self.conn.cursor()
+        self.conn = sqlite3.connect('Iteration_3/database/CourseRegistration.db')
+        self.cursor = self.conn.cursor()
         self.courseSections = self.initialize_courseSections()
 
     def print_table(self, table_name: str):        
@@ -33,6 +33,8 @@ class SqliteManager:
     def save_student(self, student: Student):
         student.get_name()
         
+        
+        
     def initialize_courseSections(self) -> list:
         courseSections = []
          # Step 1: Query all CourseSections
@@ -40,7 +42,7 @@ class SqliteManager:
         sections_data = self.cursor.fetchall()
         for section_row in sections_data:
                 section_id, capacity, course_id, lecturer_ssn = section_row
-
+                
                 # Step 2: Fetch Parent Course Information
                 self.cursor.execute("SELECT courseID, name, credit, prerequisiteID, courseType FROM Course WHERE courseID = ?;", (course_id,))
                 parent_course_row = self.cursor.fetchone()
@@ -49,7 +51,7 @@ class SqliteManager:
                     course_id, name, credit, prerequisite_id, course_type = parent_course_row
                     parent_course = Course(course_id, name, credit, prerequisite_id, course_type)
         
-                        # Step 3: Fetch TimeSlots for the Section
+                # Step 3: Fetch TimeSlots for the Section
                 self.cursor.execute("SELECT day, timeInterval, classroom FROM TimeSlot WHERE sectionID = ?;", (section_id,))
                 time_slots_data = self.cursor.fetchall()
                 time_slots = [TimeSlot(day=row[0], time_interval=row[1], classroom=row[2]) for row in time_slots_data]
@@ -62,7 +64,7 @@ class SqliteManager:
                     lecturer=None  # Lecturer object can be populated later if needed
                 )
                 course_section.set_time_slots(time_slots)
-
+                courseSections.append(course_section)
                 # Step 5: Add CourseSection to the List
         return courseSections
 
@@ -72,4 +74,3 @@ class SqliteManager:
 
 
 manager = SqliteManager()
-print(manager.find_user("o150121032", "password2"))
