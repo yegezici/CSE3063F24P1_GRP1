fd3265e5c78ed023faaed55ff7f0c08fabaee8a2
Yunus Emre Gezici <120037449+yegezici@users.noreply.github.com>
2024-12-17 16:17:50 +0300

    Update users_table.sql

--- Iteration_3/SQL_queries/users_table.sql
+++ Iteration_3/SQL_queries/users_table.sql
@@ -17,3 +17,31 @@ INSERT INTO users VALUES('o150122039','password9')
 INSERT INTO users VALUES('o150122040','password10')
 INSERT INTO users VALUES('o150122041','password11')
 INSERT INTO users VALUES('o150122042','password12')
+
+
+CREATE TABLE Student (
+    studentID NVARCHAR(50) PRIMARY KEY,
+    name NVARCHAR(50),
+    surname NVARCHAR(50),
+    gender CHAR,
+    birthDate DATE,
+    advisorID NVARCHAR(50),
+	foreign key (advisorID) references Advisor(advisorID)
+);
+
+
+CREATE TABLE Lecturer (
+    ssn NVARCHAR(50) PRIMARY KEY,
+    name NVARCHAR(50),
+    surname NVARCHAR(50),
+    birthdate DATE,
+    gender CHAR(1)
+);
+
+CREATE TABLE Advisor (
+    advisorID NVARCHAR(50) PRIMARY KEY, 
+    ssn NVARCHAR(50),            
+    FOREIGN KEY (ssn) REFERENCES Lecturer(ssn)
+);
+
+
