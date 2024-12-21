cfaa0600b04c2e2a51c610a5d2801541e8e679d7
hasanerzz <m.h.erzincanli@gmail.com>
2024-12-22 01:38:34 +0300

    Admin and AdminInterface updated

--- Iteration_3/Admin.py
+++ Iteration_3/Admin.py
@@ -5,79 +5,124 @@ from typing import List,Optional
 from datetime import date
 class Admin(Staff):
     def __init__(self, 
-                 name: str = "", 
-                 surname: str = "", 
-                 birthdate: Optional[date] = None, 
-                 ssn: str = "", 
+                 _name: str = "", 
+                 _surname: str = "", 
+                 _birthdate: Optional[date] = None, 
+                 _ssn: str = "", 
+                 _gender: str = "",
                  students: Optional[List['Student']] = None, 
                  advisors: Optional[List['Advisor']] = None, 
                  lecturers: Optional[List['Lecturer']] = None, 
                  department_schedulers: Optional[List['DepartmentScheduler']] = None):
-        self.name = name
-        self.surname = surname
-        self.birthdate = birthdate
-        self.ssn = ssn
-        self.students = students if students is not None else []
-        self.advisors = advisors if advisors is not None else []
-        self.lecturers = lecturers if lecturers is not None else []
-        self.department_schedulers = department_schedulers if department_schedulers is not None else []
+        super().__init__(name=_name, surname=_surname, birthdate=_birthdate, gender=_gender, ssn=_ssn)
+        self._students = students if students is not None else []
+        self._advisors = advisors if advisors is not None else []
+        self._lecturers = lecturers if lecturers is not None else []
+        self._department_schedulers = department_schedulers if department_schedulers is not None else []
     
     def add_student(self, student):
         try:
             #SqliteManager.save_student(student)
-            self.students.append(student)
+            self._students.append(student)
         except Exception as e:
             logger.warning(str(e))
     
     def delete_student(self, student_id_to_be_deleted: str):
         try:
             #SqliteManager.delete_student(student)
-            for student in self.students:
+            for student in self._students:
                 if student.get_id() == student_id_to_be_deleted:
-            self.students.remove(student)
+                    self._students.remove(student)
         except Exception as e:
             logger.warning(str(e))
     
     def add_advisor(self, advisor):
         try:
             #SqliteManager.add_advisor(advisor)
-            self.advisors.append(advisor)
+            self._advisors.append(advisor)
         except Exception as e:
             logger.warning(str(e))
 
-    def delete_advisor(self, advisor):
+    def delete_advisor(self, advisor_id):
         try:
-            #SqliteManager.delete_advisor(advisor)
-            self.advisors.remove(advisor)
+            for advisor in self._advisors:
+                if advisor.get_id() == advisor_id:
+                    self._advisors.remove(advisor)
         except Exception as e:
             logger.warning(str(e))
 
     def add_lecturer(self, lecturer):
         try:
             #SqliteManager.add_lecturer(lecturer)
-            self.lecturers.append(lecturer)
+            self._lecturers.append(lecturer)
         except Exception as e:
             logger.warning(str(e))
     
     def delete_lecturer(self, lecturer):
         try:
             #SqliteManager.delete_lecturer(lecturer)
-            self.lecturers.remove(lecturer)
+            for lecturer in self._lecturers:
+                if lecturer.get_id() == lecturer.get_id():
+                    self._lecturers.remove(lecturer)
         except Exception as e:
             logger.warning(str(e))
     
     def add_department_scheduler(self, department_scheduler):
         try:
             #SqliteManager.add_department_scheduler(department_scheduler)
-            self.department_schedulers.append(department_scheduler)
+            self._department_schedulers.append(department_scheduler)
         except Exception as e:
             logger.warning(str(e))
     
     def delete_department_scheduler(self, department_scheduler):
         try:
             #SqliteManager.delete_department_scheduler(department_scheduler)
-            self.department_schedulers.remove(department_scheduler)
+            for department_scheduler in self._department_schedulers:
+                if department_scheduler.get_id() == department_scheduler.get_id():
+                    self._department_schedulers.remove(department_scheduler)
         except Exception as e:
             logger.warning(str(e))
 
+   # Getter and Setter for name
+    def get_name(self):
+        return super().get_name()
+
+    # Getter and Setter for surname
+    def get_surname(self):
+        return super().get_surname()
+
+    # Getter and Setter for birthdate
+    def get_birthdate(self):
+        return super().get_birthdate()
+
+    # Getter and Setter for ssn
+    def get_ssn(self):
+        return super().get_ssn()
     
+     # Getter and Setter for students
+    def get_students(self):
+        return self._students
+
+    def set_students(self, students):
+        self._students = students
+
+    # Getter and Setter for advisors
+    def get_advisors(self):
+        return self._advisors
+
+    def set_advisors(self, advisors):
+        self._advisors = advisors
+
+    # Getter and Setter for lecturers
+    def get_lecturers(self):
+        return self._lecturers
+
+    def set_lecturers(self, lecturers):
+        self._lecturers = lecturers
+
+    # Getter and Setter for department_schedulers
+    def get_department_schedulers(self):
+        return self._department_schedulers
+
+    def set_department_schedulers(self, department_schedulers):
+        self._department_schedulers = department_schedulers
--- Iteration_3/AdminInterface.py
+++ Iteration_3/AdminInterface.py
@@ -1,47 +1,55 @@
 from Admin import Admin
-from Student import Student
 from typing import List
 from SQLiteManagement import SQLiteManagement
 from Logging_Config import logger
 
 class AdminInterface:
-    __init__(self, admin: Admin, advisors: List[Advisor]):
+    def __init__(self, admin: Admin, course_sections: List['CourseSection']):
         self.admin = admin
-        self.advisors = advisors
+        self.course_sections = course_sections
+        
     
     def show_menu(self):
         log_out = False
         choice = self.get_choice()
         if choice == 1:
             logger.info(f"Choice 1:  Add Student is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
-            student_id, student_name, student_surname, student_birthdate, student_AdvisorID = self.student_to_add()
-            for advisor in self.advisors:
-                if advisor.get_id() == student_AdvisorID:
-                    student = Student(student_id, student_name, student_surname, student_birthdate)
-                    advisor.add_student(student)
-                    student.set_advisor(advisor)
-                    break
-            student = Student(student_id, student_name, student_surname, student_birthdate)
+            student = self.student_to_add()
             self.admin.add_student(student)
-            SQLiteManagement.add_student(student)
+            #SQLiteManagement.add_student(student)
         elif choice == 2:
             logger.info(f"Choice 2:  Delete Student is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
             student_id_to_be_deleted = input("Enter student ID to be deleted: ")
             self.admin.delete_student(student_id_to_be_deleted)
-            SQLiteManagement.delete_student(student_id_to_be_deleted)
+            #SQLiteManagement.delete_student(student_id_to_be_deleted)
         elif choice == 3:
             logger.info(f"Choice 3:  Add Advisor is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
-            self.admin.add_advisor()
+            advisor = self.advisor_to_add()
+            self.admin.add_advisor(advisor)
+            #SQLiteManagement.add_advisor(advisor)
         elif choice == 4:
             logger.info(f"Choice 4:  Delete Advisor is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
+            advisor_id_to_be_deleted = input("Enter student ID to be deleted: ")
+            self.admin.delete_student(advisor_id_to_be_deleted)
+            #SQLiteManagement.delete_student(advisor_id_to_be_deleted)
             self.admin.delete_advisor()
         elif choice == 5:
             logger.info(f"Choice 5:  Add Lecturer is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
-            self.admin.add_lecturer()
+            lecturer = self.lecturer_to_add()
+            self.admin.add_lecturer(lecturer)
         elif choice == 6:
             logger.info(f"Choice 6:  Delete Lecturer is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
-            self.admin.delete_lecturer()
+            lecturer_id_to_be_deleted = input("Enter lecturer ID to be deleted: ")
+            self.admin.delete_lecturer(lecturer_id_to_be_deleted)
         elif choice == 7:
+            logger.info(f"Choice 7: Add Department Scheduler is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
+            scheduler = self.scheduler_to_add()
+            self.admin.add_department_scheduler(scheduler)
+        elif choice == 8:
+            logger.info(f"Choice 8: Delete Department Scheduler is selected     - {self.admin.get_name()} {self.admin.get_surname()}")
+            scheduler_id_to_be_deleted = input("Enter scheduler ID to be deleted: ")
+            self.admin.delete_department_scheduler(scheduler_id_to_be_deleted)
+        elif choice == 9:
             logger.info(f"{self.admin.get_name()} {self.admin.get_surname()} succesfully logged out.")
             log_out = True
         else:
@@ -51,7 +59,7 @@ class AdminInterface:
 
     
     def get_choice(self):
-        print("Select an operation:\n1. Add Student\n2. Delete Student\n3. Add Advisor\n4. Delete Advisor\n5. Add Lecturer\n6. Delete Lecturer\n7. Log out")
+        print("Select an operation:\n1. Add Student\n2. Delete Student\n3. Add Advisor\n4. Delete Advisor\n5. Add Lecturer\n6. Delete Lecturer\n7. Add Department Scheduler\n8. Log out")
         try:
             choice = int(input("Enter your choice: "))
             return choice
@@ -64,6 +72,53 @@ class AdminInterface:
         student_id = input("Enter student ID: ")
         student_name = input("Enter student name: ")
         student_surname = input("Enter student surname: ")
+        student_gender = input("Enter student gender:")
         student_birthdate = input("Enter student birthdate: ")
-        student_AdvisorID = input("Enter student advisorID: ")
-        return student_id, student_name, student_surname, student_birthdate, student_AdvisorID
+        student_advisorID = input("Enter student advisorID: ")
+        from Student import Student
+        for advisor in self.admin.get_advisors():
+                if advisor.get_id() == student_advisorID:
+                    from Transcript import Transcript
+                    transcript = Transcript()
+                    student = Student(name=student_name, surname=student_surname, birthdate=student_birthdate, gender=student_gender, transcript=transcript, student_id=student_id)
+                    advisor(student)
+                    student.set_advisor(advisor)
+                    break
+        return student
+
+    def advisor_to_add(self):
+        lecturer_id = input("Enter lecturer ID to be promoted to Advisor: ")
+        lecturer = self.find_lecturer_by_id(lecturer_id)
+        if lecturer:
+            from Advisor import Advisor
+            advisor = Advisor(ssn=lecturer.get_id(),name=lecturer.get_name(), surname=lecturer.get_surname(), birthdate=lecturer.get_birthdate(), gender=lecturer.get_gender(), courses=None, students=None)
+            return advisor
+        else:
+            logger.warning("Lecturer ID not found.")
+            return None
+    
+
+    def find_lecturer_by_id(self, lecturer_id: str):
+        for lecturer in self.admin.get_lecturers():
+            if lecturer.get_id() == lecturer_id:
+                return lecturer
+        return None
+
+    def lecturer_to_add(self):
+        lecturer_id = input("Enter lecturer ID: ")
+        lecturer_name = input("Enter lecturer name: ")
+        lecturer_surname = input("Enter lecturer surname: ")
+        lecturer_gender = input("Enter lecturer gender: ")
+        lecturer_birthdate = input("Enter lecturer birthdate: ")
+        from Lecturer import Lecturer
+        lecturer = Lecturer(id=lecturer_id, name=lecturer_name, surname=lecturer_surname,gender=lecturer_gender,courses=None,birthdate=lecturer_birthdate)
+        return lecturer
+
+
+    def scheduler_to_add(self):
+        lecturer_id = input("Enter scheduler ID to be promoted to Department Scheduler: ")
+        lecturer = self.find_lecturer_by_id(lecturer_id)
+        if lecturer:
+            from DepartmentScheduler import DepartmentScheduler
+            scheduler = DepartmentScheduler(id=lecturer.get_id(), name=lecturer.get_name(), surname=lecturer.get_surname(), birthdate=lecturer.get_birthdate, gender=lecturer.get_gender() ,course_sections=self.course_sections, all_time_intervals=None,)
+        return scheduler
