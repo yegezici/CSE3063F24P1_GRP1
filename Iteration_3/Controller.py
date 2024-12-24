bf9a792382779a78f540df64f281bc2ff7693890
Cihat Emre Vardiş <133539020+cihatemrevrds@users.noreply.github.com>
2024-12-24 17:11:34 +0300

    Delete Iteration_3 directory

--- Iteration_3/Controller.py
+++
@@ -1,9 +0,0 @@
-from CourseRegistration import CourseRegistration
-
-class Controller:
-    def main():
-        course_reg = CourseRegistration()
-        course_reg.init()
-
-if __name__ == "__main__":
-    Controller.main()
